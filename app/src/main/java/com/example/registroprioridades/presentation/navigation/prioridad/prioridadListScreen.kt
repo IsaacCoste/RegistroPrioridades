dc4504c81ca079098c55cfb7697a0ae6c10d7ac7
IsaacCoste <isaac24.coste@gmail.com>
2024-09-08 08:53:12 -0400

    Implemente el Lista de prioridades y le agrege navegacion

---
+++ app/src/main/java/com/example/registroprioridades/presentation/navigation/prioridad/prioridadListScreen.kt
@@ -0,0 +1,147 @@
+package com.example.registroprioridades.presentation.navigation.prioridad
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.registroprioridades.data.entities.PrioridadEntity
+
+@Composable
+fun PrioridadListScreen(
+    prioridadList: List<PrioridadEntity>,
+    createPrioridad: () -> Unit,
+    onEditPrioridad: (PrioridadEntity) -> Unit,
+    onDeletePrioridad: (PrioridadEntity) -> Unit
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Spacer(modifier = Modifier.padding(16.dp))
+            Text(
+                text = "Lista de Prioridades",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.padding(20.dp))
+            Row(
+                modifier = Modifier
+            ) {
+                Text(
+                    text = "Descripción",
+                    modifier = Modifier.weight(2f),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Días de Compromiso",
+                    modifier = Modifier.weight(2f),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(prioridadList) { prioridad ->
+                    ListRow(prioridad, onEditPrioridad, onDeletePrioridad)
+                }
+            }
+        }
+
+        FloatingActionButton(
+            onClick = createPrioridad,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(vertical = 50.dp, horizontal = 16.dp),
+            containerColor = MaterialTheme.colorScheme.tertiary,
+            contentColor = Color.White
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Agregar Prioridad")
+        }
+    }
+}
+
+@Composable
+fun ListRow(
+    prioridad: PrioridadEntity,
+    onEditPrioridad: (PrioridadEntity) -> Unit,
+    onDeletePrioridad: (PrioridadEntity) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Column {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { expanded = true }
+        ) {
+            Text(
+                text = prioridad.descripcion,
+                modifier = Modifier.weight(2f),
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = prioridad.diasCompromiso.toString(),
+                modifier = Modifier.weight(2f),
+                textAlign = TextAlign.Center
+            )
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = { Text("Editar") },
+                onClick = {
+                    expanded = false
+                    onEditPrioridad(prioridad)
+                }
+            )
+            DropdownMenuItem(
+                text = { Text("Eliminar") },
+                onClick = {
+                    expanded = false
+                    onDeletePrioridad(prioridad)
+                }
+            )
+        }
+        Divider(color = Color.Gray, thickness = 1.dp, modifier = Modifier.padding(vertical = 8.dp))
+    }
+}
