789df93e830ad6627fa6524c6e9612521f9d3404
IsaacCoste <isaac24.coste@gmail.com>
2024-09-08 08:59:02 -0400

    Optimize los import

--- app/src/main/java/com/example/registroprioridades/MainActivity.kt
+++ app/src/main/java/com/example/registroprioridades/MainActivity.kt
@@ -4,33 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import com.example.registroprioridades.data.database.PrioridadDb
-import com.example.registroprioridades.data.entities.PrioridadEntity
 import com.example.registroprioridades.presentation.navigation.PrioridadNavHost
-import com.example.registroprioridades.presentation.navigation.prioridad.PrioridadScreen
 import com.example.registroprioridades.ui.theme.RegistroPrioridadesTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private lateinit var prioridadDb: PrioridadDb
