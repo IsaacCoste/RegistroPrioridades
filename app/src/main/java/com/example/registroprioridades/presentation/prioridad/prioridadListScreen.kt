461347ac64d05fff149d4d59dba6c047b4eaf6f5
IsaacCoste <isaac24.coste@gmail.com>
2024-09-17 17:41:12 -0400

    Adate para usar ViewModel

--- app/src/main/java/com/example/registroprioridades/presentation/prioridad/prioridadListScreen.kt
+++ app/src/main/java/com/example/registroprioridades/presentation/prioridad/prioridadListScreen.kt
@@ -4,94 +4,104 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.registroprioridades.data.entities.PrioridadEntity
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.registroprioridades.data.local.entities.PrioridadEntity
 
 @Composable
 fun PrioridadListScreen(
-    prioridadList: List<PrioridadEntity>,
+    viewModel: PrioridadViewModel = hiltViewModel(),
     createPrioridad: () -> Unit,
-    onEditPrioridad: (PrioridadEntity) -> Unit,
-    onDeletePrioridad: (PrioridadEntity) -> Unit
+    onEditPrioridad: (Int) -> Unit,
+    onDeletePrioridad: (Int) -> Unit
 ) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        ) {
-            Spacer(modifier = Modifier.padding(16.dp))
-            Text(
-                text = "Lista de Prioridades",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp),
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.primary,
-                textAlign = TextAlign.Center
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    PrioridadListBodyScreen(
+        uiState = uiState,
+        createPrioridad = createPrioridad,
+        onEditPrioridad = onEditPrioridad,
+        onDeletePrioridad = onDeletePrioridad
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PrioridadListBodyScreen(
+    uiState: UiState,
+    createPrioridad: () -> Unit,
+    onEditPrioridad: (Int) -> Unit,
+    onDeletePrioridad: (Int) -> Unit
+) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Lista de Prioridades") },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(                )
             )
-            Spacer(modifier = Modifier.padding(20.dp))
-            Row(
-                modifier = Modifier
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = createPrioridad,
+                containerColor = MaterialTheme.colorScheme.tertiary,
+                contentColor = Color.White
             ) {
-                Text(
-                    text = "Descripción",
-                    modifier = Modifier.weight(2f),
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.Bold
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Agregar Prioridad"
                 )
-                Text(
-                    text = "Días de Compromiso",
-                    modifier = Modifier.weight(2f),
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(16.dp)
-            ) {
-                items(prioridadList) { prioridad ->
-                    ListRow(prioridad, onEditPrioridad, onDeletePrioridad)
-                }
             }
         }
-
-        FloatingActionButton(
-            onClick = createPrioridad,
+    ) { innerPadding ->
+        Column(
             modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(vertical = 50.dp, horizontal = 16.dp),
-            containerColor = MaterialTheme.colorScheme.tertiary,
-            contentColor = Color.White
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp)
         ) {
-            Icon(imageVector = Icons.Default.Add, contentDescription = "Agregar Prioridad")
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(vertical = 16.dp)
+            ) {
+                items(uiState.prioridades) { prioridad ->
+                    ListRow(
+                        prioridad = prioridad,
+                        onEditPrioridad = onEditPrioridad,
+                        onDeletePrioridad = onDeletePrioridad
+                    )
+                }
+            }
         }
     }
 }
@@ -99,30 +109,34 @@ fun PrioridadListScreen(
 @Composable
 fun ListRow(
     prioridad: PrioridadEntity,
-    onEditPrioridad: (PrioridadEntity) -> Unit,
-    onDeletePrioridad: (PrioridadEntity) -> Unit
+    onEditPrioridad: (Int) -> Unit,
+    onDeletePrioridad: (Int) -> Unit
 ) {
     var expanded by remember { mutableStateOf(false) }
 
     Column {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .clickable { expanded = true }
+                .padding(16.dp)
         ) {
-            Text(
-                text = prioridad.descripcion,
-                modifier = Modifier.weight(2f),
-                textAlign = TextAlign.Center
-            )
-            Text(
-                text = prioridad.diasCompromiso.toString(),
-                modifier = Modifier.weight(2f),
-                textAlign = TextAlign.Center
-            )
+            Column {
+                Text(
+                    text = "ID: " + prioridad.prioridadId.toString(),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "Descripción: " + prioridad.descripcion,
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "Días de Compromiso: " + prioridad.diasCompromiso.toString(),
+                    textAlign = TextAlign.Center
+                )
+            }
         }
-
         DropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false }
@@ -131,14 +145,14 @@ fun ListRow(
                 text = { Text("Editar") },
                 onClick = {
                     expanded = false
-                    onEditPrioridad(prioridad)
+                    onEditPrioridad(prioridad.prioridadId!!)
                 }
             )
             DropdownMenuItem(
                 text = { Text("Eliminar") },
                 onClick = {
                     expanded = false
-                    onDeletePrioridad(prioridad)
+                    onDeletePrioridad(prioridad.prioridadId!!)
                 }
             )
         }
