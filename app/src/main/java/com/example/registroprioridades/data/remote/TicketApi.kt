f5917f885bb6f928fa741a83ac391d2084f27a37
IsaacCoste <isaac24.coste@gmail.com>
2024-10-08 14:34:56 -0400

    Agrega el interfaz de TicketApi

---
+++ app/src/main/java/com/example/registroprioridades/data/remote/TicketApi.kt
@@ -0,0 +1,23 @@
+package com.example.registroprioridades.data.remote
+
+import com.example.registroprioridades.data.remote.dto.TicketDto
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface TicketApi {
+
+    @GET("api/Tickets/{id}")
+    suspend fun getTickets(@Path("id") id: Int): TicketDto
+
+    @GET("api/Tickets")
+    suspend fun getAllTicket(): List<TicketDto>
+
+    @POST("api/Tickets")
+    suspend fun saveTicket(@Body ticketDto: TicketDto?): TicketDto
+
+    @DELETE("api/Tickets/{id}")
+    suspend fun deleteTicket(@Path("id") id: Int)
+}
