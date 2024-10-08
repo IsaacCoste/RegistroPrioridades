901bc3419debf1f904571a98bdf64ee06abcac04
IsaacCoste <isaac24.coste@gmail.com>
2024-10-08 14:40:17 -0400

    Agrega el ClienteRepository

---
+++ app/src/main/java/com/example/registroprioridades/data/repository/ClienteRepository.kt
@@ -0,0 +1,18 @@
+package com.example.registroprioridades.data.repository
+
+import com.example.registroprioridades.data.remote.ClienteApi
+import com.example.registroprioridades.data.remote.dto.ClienteDto
+import javax.inject.Inject
+
+class ClienteRepository @Inject constructor(
+    private val clienteApi: ClienteApi
+) {
+    suspend fun saveCliente(cliente: ClienteDto) = clienteApi.saveCliente(cliente)
+
+    suspend fun getCliente(id: Int) = clienteApi.getCliente(id)
+
+    suspend fun deleteCliente(id: Int) = clienteApi.deleteCliente(id)
+
+    suspend fun getClientes() = clienteApi.getAllCliente()
+
+}
