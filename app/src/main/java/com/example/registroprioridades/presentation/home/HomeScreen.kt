db02eae9f8a28676f28e4d7a51a69adf733b162a
IsaacCoste <isaac24.coste@gmail.com>
2024-10-08 15:24:21 -0400

    Agrega el HomeScreen con los botones de la navegacion

--- app/src/main/java/com/example/registroprioridades/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/registroprioridades/presentation/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.registroprioridades.presentation.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,10 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,29 +32,57 @@ fun HomeScreen(navController: NavController) {
 
         }
     ) { innerPadding ->
-
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ){
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+                    .padding(innerPadding),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                OutlinedButton(
+                    onClick = { navController.navigate(Screen.PrioridadList) }
+                ) {
+                    Text(
+                        text = "Prioridades",
+                        modifier = Modifier.padding(8.dp),
+                    )
+                }
+                OutlinedButton(
+                    onClick = { navController.navigate(Screen.TicketList) }
+                ) {
+                    Text(
+                        text = "Tickets",
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 10.dp)
                 .padding(innerPadding),
             horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        )
-        {
+        ) {
             OutlinedButton(
-                onClick = { navController.navigate(Screen.PrioridadList) }
+                onClick = { navController.navigate(Screen.ClienteList) }
             ) {
                 Text(
-                    text = "Prioridades",
-                    modifier = Modifier.padding(8.dp),
+                    text = "Clientes",
+                    modifier = Modifier.padding(8.dp)
                 )
             }
             OutlinedButton(
-                onClick = { navController.navigate(Screen.TicketList) }
+                onClick = { navController.navigate(Screen.SistemaList) }
             ) {
                 Text(
-                    text = "Tickets",
+                    text = "Sistemas",
                     modifier = Modifier.padding(8.dp)
                 )
             }
