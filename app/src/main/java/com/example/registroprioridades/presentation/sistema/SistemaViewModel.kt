5933a64e7de3e1340f01f72cac1320ab73245d9d
IsaacCoste <isaac24.coste@gmail.com>
2024-10-08 14:45:42 -0400

    Agrega el SistemasViewModel

---
+++ app/src/main/java/com/example/registroprioridades/presentation/sistema/SistemaViewModel.kt
@@ -0,0 +1,94 @@
+package com.example.registroprioridades.presentation.sistema
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.registroprioridades.data.remote.dto.SistemaDto
+import com.example.registroprioridades.data.repository.SistemaRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SistemasViewModel @Inject constructor(
+    private val sistemasRepository: SistemaRepository
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        getSistemas()
+    }
+    fun save() {
+        viewModelScope.launch {
+            if (_uiState.value.sistemaNombre.isBlank()) {
+                _uiState.update {
+                    it.copy(errorMessage = "El sistema no puede estar vacio")
+                }
+            } else {
+                sistemasRepository.saveSistema(_uiState.value.toEntity())
+                nuevo()
+            }
+        }
+    }
+    private fun nuevo() {
+        _uiState.update {
+            it.copy(
+                sistemaId = 0,
+                sistemaNombre = "",
+                errorMessage = null
+            )
+        }
+    }
+    fun selectedSistema(sistemaId: Int) {
+        viewModelScope.launch {
+            if (sistemaId > 0) {
+                val sistema = sistemasRepository.getSistema(sistemaId)
+                _uiState.update {
+                    it.copy(
+                        sistemaId = sistema.sistemasId,
+                        sistemaNombre = sistema.sistemaNombre
+                    )
+                }
+            }
+        }
+    }
+    fun delete () {
+        viewModelScope.launch {
+            sistemasRepository.deleteSistema(_uiState.value.sistemaId!!)
+            nuevo()
+        }
+    }
+    private fun getSistemas() {
+        viewModelScope.launch {
+            val sistemas = sistemasRepository.getSistemas()
+            _uiState.update {
+                it.copy(sistemas = sistemas)
+            }
+        }
+    }
+    fun onSistemaNombreChange(sistemaNombre: String) {
+        _uiState.update {
+            it.copy(sistemaNombre = sistemaNombre)
+        }
+    }
+    fun onSistemaIdChange(sistemaId: Int) {
+        _uiState.update {
+            it.copy(sistemaId = sistemaId)
+        }
+    }
+}
+
+data class UiState(
+    val sistemaId: Int? = null,
+    val sistemaNombre: String = "",
+    val errorMessage: String? = null,
+    val sistemas: List<SistemaDto> = emptyList()
+)
+
+fun UiState.toEntity() = SistemaDto(
+    sistemasId = sistemaId,
+    sistemaNombre = sistemaNombre
+)
